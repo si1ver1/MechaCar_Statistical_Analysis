6bd0e75068c6bfacdfd10be1db3230289abe5f83
Adam <silvertitaniumx@gmail.com>
2020-10-26 21:43:46 -0700

    deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,3 +8,17 @@ head(car_mpg)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=car_mpg)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=car_mpg))
+
+
+# Deliverable 2
+coil <- read.csv(file="Suspension_Coil.csv")
+
+total_summary <- coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups= 'keep')
+
+lot_summary <- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups= 'keep')
+
+
+
+
+
+
